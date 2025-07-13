411ee0e452d64f76995ce7ba9f7a7d42127c63c3
Mohit Jadon <mohitjadoonthakur@gmail.com>
2025-07-13 11:49:12 +0530

    Make explore page and listing all users on explore page

--- MindSyncer_Server/src/routes/user.route.js
+++ MindSyncer_Server/src/routes/user.route.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { registerUser, loginUser, logoutUser, getUserById } from '../controllers/user.controller.js';
+import { registerUser, loginUser, logoutUser, getUserById,getAllUsers } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
-
+router.get('/users', getAllUsers); 
 // Yeh new route:
 router.get('/:id', getUserById);
 
